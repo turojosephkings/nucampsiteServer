5ad442857d8231fe7cc2d5995572dd68a797b68c
Arturo Reyes <turojkings@gmail.com>
2020-12-11 01:49:10 -0500

    18th Commit - OAuth with passport & Facebook

--- authenticate.js
+++ authenticate.js
@@ -3,9 +3,11 @@ const LocalStrategy = require('passport-local').Strategy;
 const User = require('./models/user');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
-const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');  //used to create, sign & verify tokens
+const FacebookTokenStrategy = require('passport-facebook-token');
 
 const config = require('./config');
+const { facebook } = require('./config');
 
 exports.local = passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
@@ -48,3 +50,34 @@ exports.verifyAdmin = (req, res, next) => {
         return next();
     }
 }
+
+exports.facebookPassport = passport.use(
+    new FacebookTokenStrategy(
+        {
+            clientID: config.facebook.clientId,
+            clientSecret: config.facebook.clientSecret
+        },
+        (accessToken, refreshToken, profile, done) => {
+            User.findOne({facebookId: profile.id}, (err, user) => {
+                if (err) {
+                    return done(err, false);
+                }
+                if (!err && user) {
+                    return done(null, user)
+                } else {
+                    user = new User({ username: profile.displayName });
+                    user.facebookId = profile.id;
+                    user.firstname = profile.name.givenName;
+                    user.lastname = profile.name.familyName;
+                    user.save((err, user) => {
+                        if (err) {
+                            return done(err, false);
+                        } else {
+                            return done(null, user)
+                        }
+                    })
+                }
+            });
+        }
+    )
+)
--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': 'YAHI-LA-PARAMBASKA-VIKTAJE',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId: '387027022529521',
+        clientSecret: '2048437e12b164f40fe65b07f88cd027'
+    }
 }
